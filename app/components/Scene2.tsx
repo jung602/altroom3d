b946118d2260cda61dbae004d10de4b1a6b51c95
jung602 <kuhaku05@naver.com>
2024-06-12 15:28:38 +0900

    0612-optest

--- app/components/Scene2.tsx
+++ app/components/Scene2.tsx
@@ -2,47 +2,20 @@
 
 import React, { Suspense, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Environment, OrbitControls, Stats, useHelper } from '@react-three/drei';
-import { SSAO, ToneMapping, Bloom } from '@react-three/postprocessing';
-import { BlurPass, Resizer, BlendFunction, KernelSize, Resolution } from 'postprocessing';
-import { EffectComposer as BaseEffectComposer } from '@react-three/postprocessing';
-import * as THREE from 'three';
+import { SSAO, ToneMapping, Bloom, EffectComposer } from '@react-three/postprocessing';
 
-import { Background } from './background';
-import { Bed } from './bed';
-import { Etc } from './etc';
-import { Desk } from './desk';
-
-import { useControls } from 'leva';  // Assuming you are using Leva for control
-import { DirectionalLight } from 'three';
-
-
-interface ExtendedEffectComposerProps extends React.ComponentProps<typeof BaseEffectComposer> {
-  smaa?: boolean;
-}
-
-const EffectComposer: React.FC<ExtendedEffectComposerProps> = (props) => {
-  // Here you can handle the smaa prop as needed, possibly passing it to a custom effect or shader
-  return <BaseEffectComposer {...props} />;
-};
-
-
+import { Altroom } from './Altroom';
 
 export default function Scene2() {
   return (
-    <Canvas orthographic camera={{ position: [45, 30, 30], zoom: 100 }} className="-z-0">
-
-
-    <ambientLight />
-
-      <Desk />
-
-      <EffectComposer smaa={true as any}>
+    <Canvas orthographic camera={{ position: [0, 0, 30], zoom: 200 }} className="-z-0">
+      <ambientLight />
+      <Altroom />
+      <EffectComposer>
             <Bloom 
                 intensity={0.03} // The bloom intensity.
             />      
       </EffectComposer>
-
-    </Canvas>
+  </Canvas>
   );
 }
